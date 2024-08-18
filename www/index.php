c5c2b9cacaef0ea1bee0bd82b52068e88d8bccad
ivan.guerrero <guerrero.ivan.789@gmail.com>
2024-08-17 20:23:40 -0500

    Added publicacion

--- www/index.php
+++ www/index.php
@@ -27,7 +27,14 @@ session_start();
             ?>
                 <div class="post">
                 <h3><?php echo htmlspecialchars($row['titulo']); ?></h3>
-                    <p><?php echo htmlspecialchars($row['contenido']); ?></p>
+                    <p><?php 
+                        // echo content max 200 characters and add ... if content is longer than 200 characters. Add Leer más link.
+                        if(strlen($row['contenido']) > 200){
+                            echo substr(htmlspecialchars($row['contenido']), 0, 200) . '... <a href="publicacion.php?id=' . $row['id'] . '">Leer más</a>';
+                        }else{
+                            echo htmlspecialchars($row['contenido']). '... <a href="publicacion.php?id=' . $row['id'] . '">Leer más</a>';
+                        }
+                    ?></p>
                     <small>Publicado por: <?php echo htmlspecialchars($row['usuario']); ?> el <?php echo htmlspecialchars($row['fecha_creacion']); ?></small>
                 </div>
             <?php endwhile;
