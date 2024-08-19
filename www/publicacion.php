316a3705124abbaee9954cb9af996b912ecebc83
ivan.guerrero <guerrero.ivan.789@gmail.com>
2024-08-19 14:30:40 -0500

    Se añade eliminar comentario, se ajusta login, registro y buscador

--- www/publicacion.php
+++ www/publicacion.php
@@ -16,28 +16,28 @@ session_start();
     <?php include_once('template/header.php'); ?>
     <main>
         <section>
-            <?php 
-                // get form url param id.
-                if(isset($_GET['id'])){
-                    include('inc/conexion.php');
-                    $query = "SELECT *, username as usuario FROM publicaciones INNER JOIN usuarios ON publicaciones.usuario_id = usuarios.id WHERE publicaciones.id = " . $_GET['id'];
-                    $result = mysqli_query($conn, $query);
-                    if(empty($result) || mysqli_num_rows($result) == 0) {
-                        echo "<p>No hay publicaciones disponibles.</p>";
-                    }else{
-                        $row = mysqli_fetch_assoc($result);
-                        ?>
-                        <div class="post">
-                            <h3><?php echo htmlspecialchars($row['titulo']); ?></h3>
-                            <p><?php echo htmlspecialchars($row['contenido']); ?></p>
-                            <small>Publicado por: <?php echo htmlspecialchars($row['usuario']); ?> el <?php echo htmlspecialchars($row['fecha_creacion']); ?></small>
-                        </div>
-                        <?php
-                        include('comentario.php');
-                    }
-                }else{
+            <?php
+            // get form url param id.
+            if (isset($_GET['id'])) {
+                include('inc/conexion.php');
+                $query = "SELECT *, username as usuario FROM publicaciones INNER JOIN usuarios ON publicaciones.usuario_id = usuarios.id WHERE publicaciones.id = " . $_GET['id'];
+                $result = mysqli_query($conn, $query);
+                if (empty($result) || mysqli_num_rows($result) == 0) {
                     echo "<p>No hay publicaciones disponibles.</p>";
+                } else {
+                    $row = mysqli_fetch_assoc($result);
+            ?>
+                    <div class="post">
+                        <h3><?php echo htmlspecialchars($row['titulo']); ?></h3>
+                        <p><?php echo htmlspecialchars($row['contenido']); ?></p>
+                        <small>Publicado por: <?php echo htmlspecialchars($row['usuario']); ?> el <?php echo htmlspecialchars($row['fecha_creacion']); ?></small>
+                    </div>
+            <?php
+                    include('comentario.php');
                 }
+            } else {
+                echo "<p>No hay publicaciones disponibles.</p>";
+            }
             ?>
         </section>
     </main>
