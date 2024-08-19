3acbbc6adb301da0cbd79ab14d59d983249e98a5
ivan.guerrero <guerrero.ivan.789@gmail.com>
2024-08-19 16:19:23 -0500

    fix header resent

--- www/login.php
+++ www/login.php
@@ -1,32 +1,5 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html leng="es">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="css/styles_vistas.css">
-</head>
-
-<body>
-    <form action="login.php" method="POST">
-        <h1>Inicio de Sesión</h1>
-        <label for="username">Nombre de Usuario:</label>
-        <input type="text" id="username" name="username" required><br>
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required><br>
-        <button type="submit">Iniciar Sesión</button>
-        <button onclick="window.location.href='index.php'">Volver al Inicio</button>
-    </form>
-</body>
-
-</html>
-
-<?php
-include 'inc/conexion.php';
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -74,4 +47,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $stmt->close();
     $conn->close();
 }
+?>
+<!DOCTYPE html>
+<html leng="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/styles_vistas.css">
+</head>
+
+<body>
+    <form action="login.php" method="POST">
+        <h1>Inicio de Sesión</h1>
+        <label for="username">Nombre de Usuario:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required><br>
+        <button type="submit">Iniciar Sesión</button>
+        <button onclick="window.location.href='index.php'">Volver al Inicio</button>
+    </form>
+</body>
+
+</html>
+
+<?php
+include 'inc/conexion.php';
+
+
 ?>
