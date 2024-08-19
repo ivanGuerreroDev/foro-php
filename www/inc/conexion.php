1293449c8b9dadbf977c04a3ff2cbdfc4ff3021b
ivan.guerrero <guerrero.ivan.789@gmail.com>
2024-08-19 12:16:37 -0500

    ajuste conexion

--- www/inc/conexion.php
+++ www/inc/conexion.php
@@ -1,14 +1,33 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'red_tracker';
+ini_set('default_charset', 'UTF-8');
+$servername = "foro-php-mysql";
+$username = "dbuser";
+$password = "dbpass";
+$dbname = "dbname";
+# $conn = new mysqli($servername, $username, $password, $dbname);
+// Create Conection to database if DB not exist create and run the script in ../db/dump.sql
 
-$conn = new mysqli($host, $user, $password, $database);
-
+$conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+  die("Conexión fallida: " . $conn->connect_error);
 }
-
-return $conn;
+$db_selected = mysqli_select_db($conn, $dbname);
+if (!$db_selected) {
+    $sql = "CREATE DATABASE $dbname CHARACTER SET utf8 COLLATE utf8_general_ci";
+    if ($conn->query($sql) === TRUE) {
+        echo "Base de datos $dbname creada exitosamente.<br>";
+        mysqli_select_db($conn, $dbname);
+        // Importar el archivo SQL
+        $dumpFile = '../db/dump.sql';
+        $command = "mysql -h $host -u $username -p$password $dbname < $dumpFile";
+        exec($command, $output, $result);
+        if ($result === 0) {
+            echo "Archivo SQL importado exitosamente.";
+        } else {
+            echo "Error al importar el archivo SQL.";
+        }
+    } else {
+        die("Error al crear la base de datos: " . $conn->error);
+    }
+}
 ?>
